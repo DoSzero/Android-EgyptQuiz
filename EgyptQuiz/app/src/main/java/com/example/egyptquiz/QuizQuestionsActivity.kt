a7de170c6ec98236b4c5eb538e50a65f0308b15a
Dima Kopyl <dimakopyl22@gmail.com>
2021-08-05 15:01:52 +0300

    Update #4
    - refactoring 
    - add fun 
    - add effects of choose

--- EgyptQuiz/app/src/main/java/com/example/egyptquiz/QuizQuestionsActivity.kt
+++ EgyptQuiz/app/src/main/java/com/example/egyptquiz/QuizQuestionsActivity.kt
@@ -1,28 +1,48 @@
 package com.example.egyptquiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition:Int = 1
+    private var mQuestionList:ArrayList<Question>? = null
+    private var mSelectedOptionPosition:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
+        mQuestionList = Constants.getQuestions()
         // Test in Logcat
-        Log.i("How many questions ??","${questionsList.size}")
+        // Log.i("How many questions ??","${questionsList.size}")
 
+        setQuestion()
+        // OnClick Listener
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+    }
 
+    private fun setQuestion(){
         // Ours Questions start ID 1 = index 0
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition -1]
+        mCurrentPosition = 1
+        //val question = mQuestionList!!.get(mCurrentPosition-1)
+        val question = mQuestionList!![mCurrentPosition-1]
 
-        progressBar.progress = currentPosition
+        defaultOptionsView()
+
+        progressBar.progress = mCurrentPosition
         // max = 10
-        tv_progress.text = "$currentPosition"+"/"+progressBar.max
+        tv_progress.text = "$mCurrentPosition"+"/"+progressBar.max
 
         // Question.kt   1) val question: String 2) others
         tv_question.text = question!!.question
@@ -32,5 +52,40 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tv_option_two.text = question.optionTwo
         tv_option_three.text = question.optionThree
         tv_option_four.text = question.optionFour
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0,tv_option_one)
+        options.add(1,tv_option_two)
+        options.add(2,tv_option_three)
+        options.add(3,tv_option_four)
+
+        // sign default user interface (Ui button)
+       for (option in options){
+           // this is default in activity_quiz_questions for TEXT
+           option.setTextColor(Color.parseColor("#7A8089"))
+           option.typeface = Typeface.DEFAULT
+           option.background = ContextCompat.getDrawable(
+               this,
+               R.drawable.default_option_border_bg
+           )
+       }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum:Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.typeface = Typeface.DEFAULT
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.default_option_border_bg
+        )
     }
 }
